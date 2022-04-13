f2b943f016cdfd5b408e869fe07bf77a5e2220e7
Rômulo Silva <romulo_lss@hotmail.com>
2022-04-13 18:46:03 -0300

    Use fallbackcontroller to show errors messages

--- lib/apielx_web/controllers/category_controller.ex
+++ lib/apielx_web/controllers/category_controller.ex
@@ -1,6 +1,9 @@
 defmodule ApielxWeb.CategoryController do
   use ApielxWeb, :controller
   alias Apielx.Categories
+  alias Apielx.Categories.Category
+
+  action_fallback ApielxWeb.FallbackController
 
   def index(conn, _params) do
     conn
@@ -13,11 +16,12 @@ defmodule ApielxWeb.CategoryController do
   end
 
   def create(conn, %{"category" => category}) do
-    {:ok, category} = Categories.create(category)
-
-    conn
-    |> put_status(:created)
-    |> put_resp_header("location", Routes.category_path(conn, :show, category))
-    |> render("show.json", category: category)
+    with {:ok, %Category{} = category} <-
+           Categories.create(category) do
+      conn
+      |> put_status(:created)
+      |> put_resp_header("location", Routes.category_path(conn, :show, category))
+      |> render("show.json", category: category)
+    end
   end
 end
---
+++ lib/apielx_web/views/changeset_view.ex
@@ -0,0 +1,8 @@
+defmodule ApielxWeb.ChangesetView do
+  use ApielxWeb, :view
+
+  def render("error.json", %{changeset: changeset}) do
+    errors = Ecto.Changeset.traverse_errors(changeset, &translate_error/1)
+    %{errors: errors}
+  end
+end
